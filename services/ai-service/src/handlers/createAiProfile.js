76643e741311ef31845013f31bae0205db46deb9
Eric Masi <eric.masi90@gmail.com>
2021-03-13 17:16:10 -0500

    Create new ai profile

--- services/ai-service/src/handlers/createAiProfile.js
+++ services/ai-service/src/handlers/createAiProfile.js
@@ -1,7 +1,27 @@
+import { DynamoDB } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
 import { commonMiddleware, successResponse } from 'libs';
+
+const dynamoDb = new DynamoDB.DocumentClient();
 
 async function createAiProfile(event, context) {
   try {
+    const now = new Date().toISOString();
+
+    const profile = {
+      id: uuid(),
+      created: now,
+      lastActionTime: now,
+    };
+
+    const params = {
+      TableName: process.env.AI_PROFILES_TABLE_NAME,
+      Item: profile,
+    };
+
+    const res = await dynamoDb.put(params).promise();
+    console.log(res);
+
     return successResponse({ message: 'AI Profile created!' });
   } catch (error) {
     console.log(error);
