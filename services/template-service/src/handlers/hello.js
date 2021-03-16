999d8070c25bb6024f0757a412980b0a0fd6169e
Eric Masi <47583837+git-masi@users.noreply.github.com>
2021-03-15 20:13:58 -0400

    Create ai profile (#7)
    * Create new ai profile
    
    * Add ai actions table
    
    * Create ai profile with values for decision calculations
    
    * Refactor to make random value utils global

--- services/template-service/src/handlers/hello.js
+++ services/template-service/src/handlers/hello.js
@@ -1,20 +1,20 @@
 // Modules
-import createHttpError from 'http-errors';
+// import createHttpError from 'http-errors';
 
 // Utils
-import { generateRandomBoolean } from '../utils/generateRandom';
+// import { generateRandomBoolean } from '../utils/generateRandom';
 
 // Libs
 import { commonMiddleware } from '../lib/commonMiddleware';
 import { successResponse } from '../lib/successResponse';
 
-function throwError() {
-  throw new createHttpError.BadRequest('Oops you made a mistake');
-}
+// function throwError() {
+//   throw new createHttpError.BadRequest('Oops you made a mistake');
+// }
 
 async function hello(event, context) {
   try {
-    if (generateRandomBoolean()) throwError();
+    // if (generateRandomBoolean()) throwError();
 
     return successResponse({ message: 'Hello from the test lambda!' });
   } catch (error) {
