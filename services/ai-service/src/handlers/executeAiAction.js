8aeda793f3650fa6bc64d918027d45b7fe6f3092
Eric Masi <eric.masi90@gmail.com>
2021-03-23 09:19:13 -0400

    Get next ai profile to create an action for

--- services/ai-service/src/handlers/executeAiAction.js
+++ services/ai-service/src/handlers/executeAiAction.js
@@ -1,27 +1,50 @@
-// import { DynamoDB } from 'aws-sdk';
-// import { v4 as uuid } from 'uuid';
-import { getMostRecentItem } from '../utils/getMostRecentItem';
+import { DynamoDB } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+import {
+  getFirstItemCreated,
+  getMostRecentItem,
+} from '../utils/queryItemsByStatusAndCreatedGSI';
 
-const { AI_ACTIONS_TABLE_NAME } = process.env;
-// const dynamoDb = new DynamoDB.DocumentClient();
+const { AI_ACTIONS_TABLE_NAME, AI_PROFILES_TABLE_NAME } = process.env;
+const dynamoDb = new DynamoDB.DocumentClient();
 
-// const createAiActionParams = {
-//   TableName: AI_ACTIONS_TABLE_NAME,
-//   Item: {
-//     id: uuid(),
-//     created: new Date().toISOString(),
-//     status: statuses.completed,
-//   },
-// };
+const createAiActionParams = {
+  TableName: AI_ACTIONS_TABLE_NAME,
+  Item: {
+    id: uuid(),
+    created: new Date().toISOString(),
+    // status: statuses.completed,
+  },
+};
 
-async function executeAiAction(event, context) {
+console.log(createAiActionParams);
+
+export const handler = async function executeAiAction(event, context) {
   try {
-    const { Items } = await getMostRecentItem(AI_ACTIONS_TABLE_NAME);
-    console.log(Items);
+    const { Items } = await getNextAiProfile();
+    const aiProfile = Items[0];
+    console.log(aiProfile);
   } catch (error) {
     console.log(error);
     throw error;
   }
-}
+};
 
-export const handler = executeAiAction;
+async function getNextAiProfile() {
+  const { Items } = await getMostRecentItem(AI_ACTIONS_TABLE_NAME);
+
+  if (Items[0]) {
+    // todo: destructure userId as well
+    const { nextAiId } = Items[0];
+    return dynamoDb
+      .get({
+        TableName: AI_PROFILES_TABLE_NAME,
+        Key: {
+          id: nextAiId,
+        },
+      })
+      .promise();
+  }
+
+  return getFirstItemCreated(AI_PROFILES_TABLE_NAME);
+}
