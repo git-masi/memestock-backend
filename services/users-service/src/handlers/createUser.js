972c13457167143bbd10656383bb962a0c820d41
Eric Masi <47583837+git-masi@users.noreply.github.com>
2021-03-25 17:29:12 -0400

    Create user refactor (#12)
    * Remove unused sort key
    
    * Add utils for working with regexp
    
    * Add axios to call other services
    
    * Add utils for generating more random values
    
    * Update table to us more generic primary key
    
    * Create a user and ACID transaction to insert in DB
    
    * Specify KeyType to fix bug
    
    * Use correct db method to fix bug

--- services/users-service/src/handlers/createUser.js
+++ services/users-service/src/handlers/createUser.js
@@ -1,42 +1,132 @@
-// CREATE
-// There should be a route to create a user
-// The route should accept a unique displayName (there should be a reasonable limit to the length)
-// A user should be saved in the Users table
-// A new UUID should be generated
-// A created time should be added as an ISO string
-// A randomly generated amount of starting cash should be added (there should be a reasonable min/max)
-// A randomly generated number of starting stocks should be added (there should be a reasonable min/max)
-
 // Modules
 import { DynamoDB } from 'aws-sdk';
 import { v4 as uuid } from 'uuid';
+import axios from 'axios';
 
-import { commonMiddleware, successResponse } from 'libs';
+import {
+  commonMiddlewareWithValidator,
+  emailRegex,
+  successResponse,
+  getRandomValueFromArray,
+  getRandomIntZeroToX,
+  getRandomIntMinToMax,
+} from 'libs';
 
-const dynamoDB = new DynamoDB.DocumentClient();
+const { USERS_TABLE_NAME, COMPANY_SERVICE_URL } = process.env;
+const dynamoDb = new DynamoDB.DocumentClient();
+const getAllCompaniesPath = `${COMPANY_SERVICE_URL}/company/all`;
+const minDollarAmountInCents = 10000; // cents
+const maxDollarAmountInCents = 500000; // cents
+const requestSchema = {
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        displayName: {
+          type: 'string',
+          minLength: 5,
+          maxLength: 36,
+          pattern: '^\\S*$',
+        },
+        email: {
+          type: 'string',
+          pattern: emailRegex.toString().replace(/\//g, ''),
+        },
+      },
+    },
+    required: { body: true },
+  },
+};
+const validationOptions = { inputSchema: requestSchema };
 
 async function createUser(event, context) {
-  console.log(process.env.USERS_TABLE_NAME);
   try {
-    const now = new Date();
-
-    const user = {
-      id: uuid(),
-      created: now.toISOString(),
-    };
-
-    const params = {
-      TableName: process.env.USERS_TABLE_NAME,
-      Item: user,
-    };
-
-    await dynamoDB.put(params).promise();
-
-    return successResponse({ message: 'User created!' });
+    const { body } = event;
+    const params = await createUserAttributes(body);
+    await dynamoDb.transactWrite(params).promise();
+    return successResponse(params.TransactItems[0].Put.Item);
   } catch (error) {
     console.log(error);
     throw error;
   }
 }
 
-export const handler = commonMiddleware(createUser);
+export const handler = commonMiddlewareWithValidator(
+  createUser,
+  validationOptions
+);
+
+async function createUserAttributes(body) {
+  const { displayName, email } = body;
+  const startingCash = getRandomIntMinToMax(
+    minDollarAmountInCents,
+    maxDollarAmountInCents
+  );
+  return {
+    TransactItems: [
+      {
+        Put: {
+          TableName: USERS_TABLE_NAME,
+          ConditionExpression: 'attribute_not_exists(pk)',
+          Item: {
+            pk: uuid(),
+            created: new Date().toISOString(),
+            displayName,
+            email,
+            stocks: await createStartingStocks(),
+            totalCash: startingCash,
+            cashOnHand: startingCash,
+          },
+        },
+      },
+      {
+        Put: {
+          TableName: USERS_TABLE_NAME,
+          ConditionExpression: 'attribute_not_exists(pk)',
+          Item: {
+            pk: `displayName#${displayName}`,
+          },
+        },
+      },
+      {
+        Put: {
+          TableName: USERS_TABLE_NAME,
+          ConditionExpression: 'attribute_not_exists(pk)',
+          Item: {
+            pk: `email#${email}`,
+          },
+        },
+      },
+    ],
+  };
+}
+
+async function createStartingStocks() {
+  const { data: companies } = await axios.get(getAllCompaniesPath);
+  const numStocks = getRandomIntMinToMax(1, companies.length);
+  const stocks = {};
+  let totalStockValue = getRandomIntMinToMax(
+    minDollarAmountInCents,
+    maxDollarAmountInCents
+  );
+
+  for (let i = 0; i < numStocks; i++) {
+    const { tickerSymbol, pk, pricePerShare } = getRandomValueFromArray(
+      companies
+    );
+    const valueHeld =
+      i === numStocks - 1
+        ? totalStockValue
+        : getRandomIntZeroToX(totalStockValue);
+    const amountHeld = Math.floor(valueHeld / pricePerShare);
+    const quantityHeld = amountHeld > 0 ? amountHeld : 1;
+
+    stocks[tickerSymbol] = {
+      id: pk,
+      quantityHeld,
+      quantityOnHand: quantityHeld,
+    };
+  }
+
+  return stocks;
+}
