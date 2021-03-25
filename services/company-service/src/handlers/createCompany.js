c96c845b2c0c93d844f225d2ee8fa0c23e57e337
Eric Masi <eric.masi90@gmail.com>
2021-03-25 15:09:19 -0400

    Save company to db

--- services/company-service/src/handlers/createCompany.js
+++ services/company-service/src/handlers/createCompany.js
@@ -1,10 +1,12 @@
 // Modules
-// import { DynamoDB } from 'aws-sdk';
+import { DynamoDB } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
 
 // Libs
 import { commonMiddlewareWithValidator, successResponse } from 'libs';
 
-// const dynamoDb = new DynamoDB.DocumentClient();
+const { COMPANIES_TABLE_NAME } = process.env;
+const dynamoDb = new DynamoDB.DocumentClient();
 const requestSchema = {
   properties: {
     body: {
@@ -36,6 +38,43 @@ const validationOptions = { inputSchema: requestSchema };
 async function createCompany(event) {
   try {
     const { body } = event;
+    const { name, tickerSymbol, description, pricePerShare } = body;
+    const params = {
+      TransactItems: [
+        {
+          Put: {
+            TableName: COMPANIES_TABLE_NAME,
+            ConditionExpression: 'attribute_not_exists(pk)',
+            Item: {
+              pk: uuid(),
+              name,
+              tickerSymbol,
+              description,
+              pricePerShare,
+            },
+          },
+        },
+        {
+          Put: {
+            TableName: COMPANIES_TABLE_NAME,
+            ConditionExpression: 'attribute_not_exists(pk)',
+            Item: {
+              pk: `companyName#${name}`,
+            },
+          },
+        },
+        {
+          Put: {
+            TableName: COMPANIES_TABLE_NAME,
+            ConditionExpression: 'attribute_not_exists(pk)',
+            Item: {
+              pk: `tickerSymbol#${name}`,
+            },
+          },
+        },
+      ],
+    };
+    await dynamoDb.transactWrite(params).promise();
     return successResponse(body);
   } catch (error) {
     console.log(error);
