bbc6b2eb7e6e8191fe5208420061b5dbb0676d64
Eric Masi <eric.masi90@gmail.com>
2021-04-08 16:15:39 -0400

    Add utils for common queries run against dynamo tables

---
+++ libs/commonQueries.js
@@ -0,0 +1,28 @@
+export function createStatusAndCreatedIndexParams(args) {
+  const {
+    tableName,
+    status,
+    limit = 10,
+    compareTime = new Date().toISOString(),
+    compareTimeOperator = '<=',
+    orderAsc = true,
+  } = args;
+
+  const params = {
+    TableName: tableName,
+    IndexName: 'statusAndCreated',
+    KeyConditionExpression: `#status = :status AND #created ${compareTimeOperator} :now`,
+    ExpressionAttributeNames: {
+      '#status': 'status',
+      '#created': 'created',
+    },
+    ExpressionAttributeValues: {
+      ':status': status,
+      ':now': compareTime,
+    },
+    Limit: limit,
+    ScanIndexForward: orderAsc,
+  };
+
+  return params;
+}
