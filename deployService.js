d6704b4958fb324c12a242510f21b9d9304cbc65
Eric Masi <eric.masi90@gmail.com>
2021-04-08 18:43:11 -0400

    Enable deploy all

--- deployService.js
+++ deployService.js
@@ -1,7 +1,7 @@
 const process = require('process');
 const fs = require('fs');
 const path = require('path');
-const { execSync } = require('child_process');
+const { execSync, exec } = require('child_process');
 
 const directory = './services/';
 const args = process.argv.slice(2);
@@ -9,6 +9,16 @@ const serviceName = args[0];
 const profile = args[1];
 const env = args[2] ?? 'dev';
 let serviceIsValid = false;
+
+if (serviceName === 'all') {
+  fs.readdirSync(directory).forEach((file) => {
+    const isDir = fs.lstatSync(path.resolve(directory, file)).isDirectory();
+    if (isDir && file.endsWith('-service')) {
+      deploy(directory + file);
+    }
+  });
+  return;
+}
 
 readDir(directory);
 
@@ -29,5 +39,14 @@ function readDir(directory) {
 function deploy(dir) {
   const awsProfile = profile ? `export AWS_PROFILE=${profile} &&` : '';
   const deployScript = `cd ${dir} && ${awsProfile} sls deploy -s ${env} -l`;
-  execSync(deployScript, { stdio: [0, 1, 2] });
+  // execSync(deployScript, { stdio: [0, 1, 2] });
+
+  exec(deployScript, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`\nstderr: ${stderr}`);
+  });
 }
