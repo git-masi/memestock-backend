21a5c71a43778814069c48f588c6cbdb11ded848
Eric Masi <eric.masi90@gmail.com>
2021-04-13 16:07:09 -0400

    Update order status and user values

--- services/order-service/src/handlers/cancelOrder.js
+++ services/order-service/src/handlers/cancelOrder.js
@@ -1,7 +1,5 @@
 import { DynamoDB } from 'aws-sdk';
 import axios from 'axios';
-import isEmpty from 'lodash.isempty';
-import createHttpError from 'http-errors';
 import { commonMiddlewareWithValidator, statuses, successResponse } from 'libs';
 
 const { ORDERS_TABLE_NAME, USER_SERVICE_URL } = process.env;
@@ -15,12 +13,8 @@ const requestSchema = {
           type: 'string',
           format: 'uuid',
         },
-        userCompletingOrder: {
-          type: 'string',
-          format: 'uuid',
-        },
       },
-      required: ['orderId', 'userCompletingOrder'],
+      required: ['orderId'],
     },
   },
   required: ['body'],
@@ -30,16 +24,12 @@ const validationOptions = { inputSchema: requestSchema };
 async function cancelOrder(event) {
   try {
     const {
-      body: { orderId, userCompletingOrder },
+      body: { orderId },
     } = event;
 
-    const orderAndUser = await getOrderAndUser(orderId, userCompletingOrder);
+    const { Item: order } = await getOrder(orderId);
 
-    console.log({ ...orderAndUser });
-    // ideally this would be an ACID transaction
-    await Promise.all([updateUsers(orderAndUser), updateOrderStatus(orderId)]);
-
-    // todo: create transaction
+    await Promise.all([updateOrderStatus(orderId), updateUser(order.userId)]);
 
     return successResponse();
   } catch (error) {
@@ -53,72 +43,31 @@ export const handler = commonMiddlewareWithValidator(
   validationOptions
 );
 
-// async function getOrderAndUser(orderId, userId) {
-//   const [{ Item: order }, user] = await Promise.all([
-//     getOrder(orderId),
-//     getUser(userId),
-//   ]);
+function getOrder(orderId) {
+  return dynamoDb
+    .get({
+      TableName: ORDERS_TABLE_NAME,
+      Key: {
+        id: orderId,
+      },
+    })
+    .promise();
+}
 
-//   if (!order || isEmpty(user)) {
-//     throw createHttpError.BadRequest(
-//       'Invalid value for orderId or userCompletingOrder'
-//     );
-//   }
+function updateUser(body) {
+  return axios.put(`${USER_SERVICE_URL}/user/cancel-order`, body);
+}
 
-//   const tickerSymbol = order.stock.tickerSymbol;
-//   const userHasStock = tickerSymbol in user.stocks;
-//   const userHasQuantityRequired =
-//     user?.stocks?.[tickerSymbol]?.quantityOnHand > order.quantity;
-
-//   if (
-//     order.orderType === 'buy' &&
-//     (!userHasStock || !userHasQuantityRequired)
-//   ) {
-//     console.log('User does not have enough stock to complete the order: ', {
-//       user,
-//       order,
-//     });
-//     throw createHttpError.BadRequest('User cannot complete this order');
-//   }
-
-//   if (order.orderType === 'sell' && user.cashOnHand < order.total) {
-//     console.log('Cash on hand too low to complete order: ', { user, order });
-//     throw createHttpError.BadRequest('User cannot complete this order');
-//   }
-
-//   return { order, user };
-// }
-
-// function getOrder(orderId) {
-//   return dynamoDb
-//     .get({
-//       TableName: ORDERS_TABLE_NAME,
-//       Key: {
-//         id: orderId,
-//       },
-//     })
-//     .promise();
-// }
-
-// async function getUser(userId) {
-//   const { data } = await axios.get(`${USER_SERVICE_URL}/user?userId=${userId}`);
-//   return data;
-// }
-
-// function updateUsers(body) {
-//   return axios.put(`${USER_SERVICE_URL}/user/complete-order`, body);
-// }
-
-// function updateOrderStatus(orderId) {
-//   return dynamoDb
-//     .update({
-//       TableName: ORDERS_TABLE_NAME,
-//       Key: { id: orderId },
-//       UpdateExpression: 'set #status = :completed',
-//       ExpressionAttributeNames: { '#status': 'status' },
-//       ExpressionAttributeValues: {
-//         ':completed': statuses.completed,
-//       },
-//     })
-//     .promise();
-// }
+function updateOrderStatus(orderId) {
+  return dynamoDb
+    .update({
+      TableName: ORDERS_TABLE_NAME,
+      Key: { id: orderId },
+      UpdateExpression: 'SET #status = :cancelled',
+      ExpressionAttributeNames: { '#status': 'status' },
+      ExpressionAttributeValues: {
+        ':cancelled': statuses.cancelled,
+      },
+    })
+    .promise();
+}
