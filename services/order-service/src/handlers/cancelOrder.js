78d33ddfb75cf63ca4d5964277dc9a96f397c6a9
Eric Masi <eric.masi90@gmail.com>
2021-04-13 16:00:12 -0400

    Add route to cancel an order

--- services/order-service/src/handlers/cancelOrder.js
+++ services/order-service/src/handlers/cancelOrder.js
@@ -27,7 +27,7 @@ const requestSchema = {
 };
 const validationOptions = { inputSchema: requestSchema };
 
-async function completeOrder(event) {
+async function cancelOrder(event) {
   try {
     const {
       body: { orderId, userCompletingOrder },
@@ -49,76 +49,76 @@ async function completeOrder(event) {
 }
 
 export const handler = commonMiddlewareWithValidator(
-  completeOrder,
+  cancelOrder,
   validationOptions
 );
 
-async function getOrderAndUser(orderId, userId) {
-  const [{ Item: order }, user] = await Promise.all([
-    getOrder(orderId),
-    getUser(userId),
-  ]);
+// async function getOrderAndUser(orderId, userId) {
+//   const [{ Item: order }, user] = await Promise.all([
+//     getOrder(orderId),
+//     getUser(userId),
+//   ]);
 
-  if (!order || isEmpty(user)) {
-    throw createHttpError.BadRequest(
-      'Invalid value for orderId or userCompletingOrder'
-    );
-  }
+//   if (!order || isEmpty(user)) {
+//     throw createHttpError.BadRequest(
+//       'Invalid value for orderId or userCompletingOrder'
+//     );
+//   }
 
-  const tickerSymbol = order.stock.tickerSymbol;
-  const userHasStock = tickerSymbol in user.stocks;
-  const userHasQuantityRequired =
-    user?.stocks?.[tickerSymbol]?.quantityOnHand > order.quantity;
+//   const tickerSymbol = order.stock.tickerSymbol;
+//   const userHasStock = tickerSymbol in user.stocks;
+//   const userHasQuantityRequired =
+//     user?.stocks?.[tickerSymbol]?.quantityOnHand > order.quantity;
 
-  if (
-    order.orderType === 'buy' &&
-    (!userHasStock || !userHasQuantityRequired)
-  ) {
-    console.log('User does not have enough stock to complete the order: ', {
-      user,
-      order,
-    });
-    throw createHttpError.BadRequest('User cannot complete this order');
-  }
+//   if (
+//     order.orderType === 'buy' &&
+//     (!userHasStock || !userHasQuantityRequired)
+//   ) {
+//     console.log('User does not have enough stock to complete the order: ', {
+//       user,
+//       order,
+//     });
+//     throw createHttpError.BadRequest('User cannot complete this order');
+//   }
 
-  if (order.orderType === 'sell' && user.cashOnHand < order.total) {
-    console.log('Cash on hand too low to complete order: ', { user, order });
-    throw createHttpError.BadRequest('User cannot complete this order');
-  }
+//   if (order.orderType === 'sell' && user.cashOnHand < order.total) {
+//     console.log('Cash on hand too low to complete order: ', { user, order });
+//     throw createHttpError.BadRequest('User cannot complete this order');
+//   }
 
-  return { order, user };
-}
+//   return { order, user };
+// }
 
-function getOrder(orderId) {
-  return dynamoDb
-    .get({
-      TableName: ORDERS_TABLE_NAME,
-      Key: {
-        id: orderId,
-      },
-    })
-    .promise();
-}
+// function getOrder(orderId) {
+//   return dynamoDb
+//     .get({
+//       TableName: ORDERS_TABLE_NAME,
+//       Key: {
+//         id: orderId,
+//       },
+//     })
+//     .promise();
+// }
 
-async function getUser(userId) {
-  const { data } = await axios.get(`${USER_SERVICE_URL}/user?userId=${userId}`);
-  return data;
-}
+// async function getUser(userId) {
+//   const { data } = await axios.get(`${USER_SERVICE_URL}/user?userId=${userId}`);
+//   return data;
+// }
 
-function updateUsers(body) {
-  return axios.put(`${USER_SERVICE_URL}/user/complete-order`, body);
-}
+// function updateUsers(body) {
+//   return axios.put(`${USER_SERVICE_URL}/user/complete-order`, body);
+// }
 
-function updateOrderStatus(orderId) {
-  return dynamoDb
-    .update({
-      TableName: ORDERS_TABLE_NAME,
-      Key: { id: orderId },
-      UpdateExpression: 'set #status = :completed',
-      ExpressionAttributeNames: { '#status': 'status' },
-      ExpressionAttributeValues: {
-        ':completed': statuses.completed,
-      },
-    })
-    .promise();
-}
+// function updateOrderStatus(orderId) {
+//   return dynamoDb
+//     .update({
+//       TableName: ORDERS_TABLE_NAME,
+//       Key: { id: orderId },
+//       UpdateExpression: 'set #status = :completed',
+//       ExpressionAttributeNames: { '#status': 'status' },
+//       ExpressionAttributeValues: {
+//         ':completed': statuses.completed,
+//       },
+//     })
+//     .promise();
+// }
