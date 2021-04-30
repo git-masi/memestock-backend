f72a58f5b55c02ff96ab51e5319c2337172f72a9
Eric Masi <eric.masi90@gmail.com>
2021-04-30 15:48:41 -0400

    Add route to send a count of transactions

--- services/transaction-service/src/handlers/getTransactionsCount.js
+++ services/transaction-service/src/handlers/getTransactionsCount.js
@@ -5,14 +5,20 @@ const { TRANSACTIONS_TABLE_NAME } = process.env;
 const dynamoDb = new DynamoDB.DocumentClient();
 
 export async function getTransactions() {
-  const params = {
-    TableName: TRANSACTIONS_TABLE_NAME,
-    Select: 'COUNT',
-  };
+  try {
+    const params = {
+      TableName: TRANSACTIONS_TABLE_NAME,
+      Select: 'COUNT',
+    };
 
-  const { Count: count } = await dynamoDb.scan(params).promise();
+    const { Count: count } = await dynamoDb.scan(params).promise();
+    console.log(count);
 
-  return successResponseCors({ count });
+    return successResponseCors({ count });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 export const handler = commonMiddleware(getTransactions);
