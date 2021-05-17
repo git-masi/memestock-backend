17197de7da88f3dccabd12162df8781edb96ad15
Eric Masi <eric.masi90@gmail.com>
2021-05-17 12:03:44 -0400

    Add transaction to put user in DB

--- v2/handlers/users.js
+++ v2/handlers/users.js
@@ -1,6 +1,14 @@
+import { DynamoDB } from 'aws-sdk';
+import { randomInt } from 'd3-random';
+import { nanoid } from 'nanoid';
 import { apiResponse, HttpError, httpMethods } from '../utils/http';
 import { commonMiddleware } from '../utils/middleware';
 import { validRequestFor } from './usersSchema';
+
+const { MAIN_TABLE_NAME } = process.env;
+const minStartingCash = 10000; // cents
+const maxStartingCash = 500000; // cents
+const dynamoDb = new DynamoDB.DocumentClient();
 
 export const handler = commonMiddleware(lambdaForUsers);
 
@@ -25,6 +33,9 @@ function route(anEvent) {
     case httpMethods.GET:
       return getUserFrom(anEvent);
 
+    case httpMethods.POST:
+      return createUserFrom(anEvent);
+
     default:
       throw HttpError.BadRequest();
   }
@@ -33,3 +44,64 @@ function route(anEvent) {
 async function getUserFrom(anEvent) {
   return { id: 1, username: 'bob' };
 }
+
+async function createUserFrom(anEvent) {
+  const {
+    body: { displayName, email },
+  } = anEvent;
+  const startingCash = getRandomInt(minStartingCash, maxStartingCash);
+  const transaction = {
+    TransactItems: [
+      {
+        Put: {
+          TableName: MAIN_TABLE_NAME,
+          Item: {
+            pk: 'USER',
+            sk: `HUMAN#${new Date().toISOString()}#${nanoid(8)}`,
+            displayName,
+            email,
+            // stocks: await createStartingStocks(),
+            stocks: {},
+            totalCash: startingCash,
+            cashOnHand: startingCash,
+          },
+        },
+      },
+      {
+        Put: {
+          TableName: MAIN_TABLE_NAME,
+          ConditionExpression: 'attribute_not_exists(pk)',
+          Item: {
+            pk: `DISPLAY_NAME#${displayName}`,
+            sk: displayName,
+          },
+        },
+      },
+      {
+        Put: {
+          TableName: MAIN_TABLE_NAME,
+          ConditionExpression: 'attribute_not_exists(pk)',
+          Item: {
+            pk: `EMAIL#${email}`,
+            sk: email,
+          },
+        },
+      },
+    ],
+  };
+  return dynamoDb.transactWrite(transaction).promise();
+}
+
+// const getRandomIntZeroToX = (x) => randomInt(0, x)();
+
+function getRandomInt(min, max) {
+  return randomInt(min, max)();
+}
+
+// const getRandomValueFromArray = (arr) =>
+//   arr instanceof Array && arr[getRandomIntZeroToX(arr.length)];
+
+// const getRandomFloat = (min, max) => randomUniform(min, max)();
+
+// const generateRandomBoolean = (probability = 0.5) =>
+//   probability > randomUniform(0, 1)();
--- v2/handlers/usersSchema.js
+++ v2/handlers/usersSchema.js
@@ -1,6 +1,15 @@
 import Ajv from 'ajv';
 import { httpMethods } from '../utils/http';
 
+export const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const emailPattern = emailRegex.toString().replace(/\//g, '');
+
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+export const passwordPattern = passwordRegex.toString().replace(/\//g, '');
+
 const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
 
 const schemas = Object.freeze({
@@ -20,8 +29,19 @@ const schemas = Object.freeze({
     properties: {
       body: {
         type: 'object',
-        // properties: {},
-        // required: [],
+        properties: {
+          displayName: {
+            type: 'string',
+            minLength: 5,
+            maxLength: 36,
+            pattern: '^\\S*$',
+          },
+          email: {
+            type: 'string',
+            pattern: emailPattern,
+          },
+        },
+        required: ['displayName', 'email'],
       },
     },
     required: ['body'],
