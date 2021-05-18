f38b6616af9c6ac3a9e8275ce22bb014961c8749
Eric Masi <eric.masi90@gmail.com>
2021-05-18 17:36:06 -0400

    Add script to seed database with companies

---
+++ v2/seedCompanies.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const dir = './events/seedCompanies';
+
+readDir(dir);
+
+function readDir(directory) {
+  fs.readdirSync(directory).forEach((file) => {
+    addToDb(`${dir}/${file}`);
+  });
+}
+
+function addToDb(path) {
+  try {
+    const script = `sls invoke local -f companies -p ${path}`;
+    execSync(script, { stdio: [0, 1, 2] });
+  } catch (error) {
+    console.info(error);
+  }
+}
