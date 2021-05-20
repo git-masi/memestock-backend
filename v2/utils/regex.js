7ad2da61e2ba9d6075a56ae70483e98d6e9b4ebd
Eric Masi <eric.masi90@gmail.com>
2021-05-20 19:42:39 -0400

    Add regex for iso string validation

--- v2/utils/regex.js
+++ v2/utils/regex.js
@@ -6,3 +6,10 @@ export const emailPattern = emailRegex.toString().replace(/\//g, '');
 export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
 export const passwordPattern = passwordRegex.toString().replace(/\//g, '');
+
+export const utcIsoStringRegex =
+  /^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$/;
+
+export const utcIsoStringPattern = utcIsoStringRegex
+  .toString()
+  .replace(/[\^$/]/g, '');
