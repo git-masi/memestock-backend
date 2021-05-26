c434e2d012bf332bac7ae3768c29b254303fc42e
Eric Masi <eric.masi90@gmail.com>
2021-05-26 11:14:05 -0400

    Add function to validate order attributes

--- v2/schema/orders.js
+++ v2/schema/orders.js
@@ -1,7 +1,15 @@
 import Ajv from 'ajv';
 import { httpMethods } from '../utils/http';
+import { createRegexGroup } from '../utils/regex';
+import { companyPkSkPattern } from './companies';
+import { userPkSkPattern } from './users';
 
 const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+const orderTypes = Object.freeze({
+  buy: 'buy',
+  sell: 'sell',
+});
 
 const httpSchemas = Object.freeze({
   [httpMethods.GET]: {
@@ -13,7 +21,7 @@ const httpSchemas = Object.freeze({
         // required: [],
       },
     },
-    required: ['body'],
+    required: ['queryStringParameters'],
   },
   [httpMethods.POST]: {
     type: 'object',
@@ -36,4 +44,32 @@ export function validOrdersHttpEvent(event) {
   function validateHttpSchema(method) {
     return ajv.compile(httpSchemas[method])(event);
   }
+}
+
+export function validOrderAttributes(orderAttributes) {
+  const schema = {
+    type: 'object',
+    properties: {
+      orderType: {
+        type: 'string',
+        pattern: `^${createRegexGroup(orderTypes)}$`,
+      },
+      userPkSk: {
+        type: 'string',
+        pattern: userPkSkPattern,
+      },
+      companyPkSk: {
+        type: '',
+        pattern: companyPkSkPattern,
+      },
+      total: {
+        type: 'integer',
+      },
+      quantity: {
+        type: 'integer',
+      },
+    },
+  };
+
+  return ajv.compile(schema)(orderAttributes);
 }
