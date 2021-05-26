d6b49a851a854345895ee2323cd0c72c1f3c52d5
Eric Masi <eric.masi90@gmail.com>
2021-05-26 11:13:28 -0400

    Add company pk/sk pattern for use in other schemas

--- v2/schema/companies.js
+++ v2/schema/companies.js
@@ -1,7 +1,12 @@
 import Ajv from 'ajv';
 import { httpMethods } from '../utils/http';
+import { pkPrefixes } from './pkPrefixes';
 
 const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+export const companySkPattern = '^[A-Z]{2,4}$';
+
+export const companyPkSkPattern = `^${pkPrefixes.company}#${companySkPattern}$`;
 
 const httpSchemas = Object.freeze({
   [httpMethods.GET]: {
@@ -27,7 +32,7 @@ const httpSchemas = Object.freeze({
           },
           tickerSymbol: {
             type: 'string',
-            minLength: 1,
+            pattern: companySkPattern,
           },
           description: {
             type: 'string',
