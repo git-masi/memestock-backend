e601a0c07f7440e079450ada7a879e77f61d7144
Eric Masi <eric.masi90@gmail.com>
2021-05-26 11:05:04 -0400

    Add file to hold pk prefixes for each document type

---
+++ v2/schema/pkPrefixes.js
@@ -0,0 +1,10 @@
+export const pkPrefixes = Object.freeze({
+  user: 'USER',
+  displayName: 'DISPLAY_NAME',
+  email: 'EMAIL',
+  userOrder: 'USER_ORDER',
+  order: 'ORDER',
+  company: 'COMPANY',
+  companyName: 'COMPANY_NAME',
+  tickerSymbol: 'TICKER_SYMBOL',
+});
