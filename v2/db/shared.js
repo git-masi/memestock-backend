1fa0f35e5d1aa9fb596d9a944916b0d467d41300
Eric Masi <eric.masi90@gmail.com>
2021-05-27 09:12:40 -0400

    Add function to get item(s) returned from db query/get

--- v2/db/shared.js
+++ v2/db/shared.js
@@ -17,3 +17,9 @@ export function getFirstItem(dbResult) {
   const result = Items?.[0] ?? Item ?? null;
   return result;
 }
+
+export function getItems(dbResult) {
+  const { Items, Item } = dbResult;
+  const result = Items ?? Item ?? null;
+  return result;
+}
