8eb40dc9c3c6becbbca67c228858c311eb138cd4
Eric Masi <eric.masi90@gmail.com>
2021-06-18 08:34:12 -0400

    Refactor to use router factories

--- v2/handlers/orders.js
+++ v2/handlers/orders.js
@@ -1,4 +1,10 @@
-import { apiResponse, HttpError, httpMethods } from '../utils/http';
+import {
+  apiResponse,
+  HttpError,
+  httpMethods,
+  methodRouter,
+  pathRouter,
+} from '../utils/http';
 import { commonMiddleware } from '../utils/middleware';
 import { createOrder, fulfillOrder } from '../db/orders';
 import { isEmpty } from '../utils/dataChecks';
@@ -6,9 +12,17 @@ import { isEmpty } from '../utils/dataChecks';
 export const handler = commonMiddleware(lambdaForOrders);
 
 async function lambdaForOrders(event) {
+  const methodRoutes = {
+    [httpMethods.POST]: handlePostMethods,
+    [httpMethods.PUT]: handlePutMethods,
+  };
+  const router = methodRouter(methodRoutes);
+
   try {
-    const result = await route(event);
+    const result = await router(event);
+
     if (isEmpty(result)) return apiResponse();
+
     return apiResponse({ body: result });
   } catch (error) {
     console.info(error);
@@ -21,25 +35,32 @@ async function lambdaForOrders(event) {
   }
 }
 
-function route(event) {
-  switch (event.httpMethod) {
-    case httpMethods.POST:
-      return createOrderFromHttpEvent(event);
+function handlePostMethods(event) {
+  const paths = {
+    '/orders': handleCreateOrder,
+  };
+  const router = pathRouter(paths);
+  const result = router(event);
 
-    case httpMethods.PUT:
-      return fulfillOrderFromHttpEvent(event);
+  return result;
 
-    default:
-      throw HttpError.BadRequest();
+  function handleCreateOrder(event) {
+    const { body } = event;
+    return createOrder(body);
   }
 }
 
-function createOrderFromHttpEvent(event) {
-  const { body } = event;
-  return createOrder(body);
-}
+function handlePutMethods(event) {
+  const paths = {
+    '/orders': handleFulfillOrder,
+  };
+  const router = pathRouter(paths);
+  const result = router(event);
 
-function fulfillOrderFromHttpEvent(event) {
-  const { body } = event;
-  return fulfillOrder(body.order, body.user);
+  return result;
+
+  function handleFulfillOrder(event) {
+    const { body } = event;
+    return fulfillOrder(body.order, body.user);
+  }
 }
