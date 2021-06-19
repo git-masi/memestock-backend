14d022e9ecbce0c4de402b73a93aaafcc45298af
Eric Masi <eric.masi90@gmail.com>
2021-06-19 10:43:12 -0400

    Enable cors for orders

--- handlers/orders.js
+++ handlers/orders.js
@@ -30,16 +30,18 @@ async function ordersLambda(event) {
   try {
     const result = await router(event);
 
-    if (isEmpty(result)) return apiResponse();
+    if (isEmpty(result)) return apiResponse({ cors: true });
 
-    return apiResponse({ body: result });
+    return apiResponse({ body: result, cors: true });
   } catch (error) {
     console.info(error);
 
-    if (error instanceof HttpError) return apiResponse({ ...error });
+    if (error instanceof HttpError)
+      return apiResponse({ ...error, cors: true });
 
     return apiResponse({
       statusCode: 500,
+      cors: true,
     });
   }
 }
